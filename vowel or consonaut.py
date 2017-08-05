3b3e4c5fed739768a647fdeb6c9576ecc519f1f9
youcantnever <30741655+youcantnever@users.noreply.github.com>
2017-08-05 11:17:14 +0530

    Create vowel or consonaut.py

---
+++ vowel or consonaut.py
@@ -0,0 +1,10 @@
+inp=raw_input('Input alphabet here: ')
+w=['a','e','i','o','u']
+if inp in w:
+    print'the given alphabet is a vowel'
+    else:
+        print'the given alphabet is consonaut'
+   
+     
+    
+
