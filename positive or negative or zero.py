435c7c91606d3e358bd950a898b2af614c11ccce
youcantnever <30741655+youcantnever@users.noreply.github.com>
2017-08-05 11:00:39 +0530

    Create positive or negative or zero.py

---
+++ positive or negative or zero.py
@@ -0,0 +1,8 @@
+inp=int(raw_input('Input number: '))
+if inp<0:
+    print 'The given number is negative'
+if inp>0:
+    print 'the given number is Postive'
+ if inp==0:
+     print 'the number is zero'
+     
